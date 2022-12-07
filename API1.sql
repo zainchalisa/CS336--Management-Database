37fc7c4d00533dc5c081fc8d16e0d1827b3e9408
Zain Chalisa <97554506+zainchalisa@users.noreply.github.com>
2022-12-07 17:34:11 -0500

    Delete API1.sql

--- API1.sql
+++
@@ -1,7 +0,0 @@
-CREATE PROCEDURE API1 (IN Candidate_Val TEXT, IN theTime TEXT, IN Precinct_Val TEXT)
-BEGIN
-Select Candidate_Val
-FROM Penna
-WHERE Timestamp = theTime and precinct = Precinct_Val;
-END
-
